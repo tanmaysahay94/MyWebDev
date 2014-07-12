3f18ca4e4d542419b37bf07c9bf71056dfede557
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-13 00:51:15 +0530

    chapter 1: urlopen with its associated methods

--- python/scripts/teach_CaptAin/teachingShraddhan.py
+++ python/scripts/teach_CaptAin/teachingShraddhan.py
@@ -6,9 +6,9 @@ proxy = {'http': MNIT_PROXY}
 #google = urlopen('http://www.google.com', proxy)
 google = urlopen('http://www.google.com')
 
-with open('file object', 'wb') as f:
+with open('file_object', 'wb') as f:
     f.write(str(google))
 
-with open('html data', 'wb') as f:
+with open('html_data', 'wb') as f:
     f.write(google.read())
 # type 'python -i teachingShraddhan.py' in the terminal

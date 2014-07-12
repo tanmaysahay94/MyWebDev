9ff6eb242c7ecec86ebb1ac569f2567588c57a54
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-13 00:48:16 +0530

    updated CaptAin package

---
+++ python/scripts/teach_CaptAin/teachingShraddhan.py
@@ -0,0 +1,14 @@
+from urllib2 import *
+
+MNIT_PROXY = 'http://172.16.1.69:3128'
+proxy = {'http': MNIT_PROXY}
+
+#google = urlopen('http://www.google.com', proxy)
+google = urlopen('http://www.google.com')
+
+with open('file object', 'wb') as f:
+    f.write(str(google))
+
+with open('html data', 'wb') as f:
+    f.write(google.read())
+# type 'python -i teachingShraddhan.py' in the terminal
--- python/scripts/teachingShraddhan.py
+++
@@ -1,9 +0,0 @@
-from urllib2 import *
-
-proxy = {'http': 'http://172.16.1.69:3128'}
-
-google = urlopen('http://www.google.com', proxy)
-
-print google
-
-# type 'python -i teachingShraddhan.py' in the terminal
